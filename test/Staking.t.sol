49028f4371b9fcf6055f3b664caf0a6b38d5724c
takez0_o <takez0_o@protonmail.com>
2022-11-09 01:32:45 +0300

    import fail

---
+++ test/Staking.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+interface Staking {
+    function balanceOf(address) external returns (uint256);
+}
+
+contract StakingTest is Test {
+    /// @dev Address of the Staking contract.
+    Staking public staking;
+
+    /// @dev Setup the testing environment.
+    function setUp() public {
+        staking = Staking(HuffDeployer.deploy("Staking"));
+    }
+
+    /// @dev Ensure that you can set and get the value.
+    function testBalanceOf() public {
+        uint256 balance = staking.balanceOf(address(this));
+        console.log("Balance:", balance);
+    }
+}
